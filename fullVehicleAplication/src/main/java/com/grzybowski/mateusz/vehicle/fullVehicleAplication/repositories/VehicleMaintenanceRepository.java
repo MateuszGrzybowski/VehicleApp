14047c822b9eac3eb45d9de13202c6b68884d2ea
Grzybatronix <mateusz.grzybowski.warszawa@gmail.com>
2020-12-11 11:54:26 +0100

    Select, Insert, Update and Delet Vehicle Movements

--- fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/repositories/VehicleMaintenanceRepository.java
+++ fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/repositories/VehicleMaintenanceRepository.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface VehicleMaintenanceRepository extends JpaRepository<VehicleMaintenance, Integer>{
 	
-	
-
+    
 }
--- fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/services/VehicleMovementService.java
+++ fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/services/VehicleMovementService.java
@@ -1,7 +1,7 @@
-/*
 package com.grzybowski.mateusz.vehicle.fullVehicleAplication.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.grzybowski.mateusz.vehicle.fullVehicleAplication.models.VehicleMovement;
 import com.grzybowski.mateusz.vehicle.fullVehicleAplication.repositories.VehicleMovementRepository;
@@ -10,14 +10,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VehicleMovementService {
-	
-	@Autowired
-	private VehicleMovementRepository vehicleMovementRepository;
-	
-	public List<VehicleMovement> findAll(){
-		return vehicleMovementRepository.findAll();
-	}	
 
+    @Autowired
+    private VehicleMovementRepository vehicleMovementRepository;
+
+    public List<VehicleMovement> findAll(){
+        return vehicleMovementRepository.findAll();
+    }
+
+    public Optional<VehicleMovement> findById(int id) {
+        return vehicleMovementRepository.findById(id);
+    }
+
+    public void delete(int id) {
+        vehicleMovementRepository.deleteById(id);
+    }
+
+    public void save(VehicleMovement vehicleMovement) {
+        vehicleMovementRepository.save(vehicleMovement);
+    }
 
 }
-*/
