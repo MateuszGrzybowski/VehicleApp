eb95f431920f63345776b25410c9ed365e872ee5
Grzybatronix <mateusz.grzybowski.warszawa@gmail.com>
2020-12-13 21:57:42 +0100

    Adding spring security configuration

--- fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/AppController.java
+++ fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/AppController.java
@@ -3,12 +3,20 @@ package com.grzybowski.mateusz.vehicle.fullVehicleAplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/*
 @Controller
-public class Controler {
+public class AppController {
 
     @GetMapping("/index")
     public String goHome(){
         return "index";
     }
-}*/
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(){
+        return "logout";
+    }
+
+}
--- fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/models/User.java
+++ fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/models/User.java
@@ -4,10 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+
 public class User {
 		
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +21,28 @@ public class User {
 	private int id;
 	private String username;
 	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
