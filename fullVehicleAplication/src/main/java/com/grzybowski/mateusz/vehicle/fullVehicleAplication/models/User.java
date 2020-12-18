998b7703966f078ddf8722d0a16991518fde393d
Grzybatronix <mateusz.grzybowski.warszawa@gmail.com>
2020-12-18 13:51:52 +0100

    fixing the bug

--- fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/models/User.java
+++ fullVehicleAplication/src/main/java/com/grzybowski/mateusz/vehicle/fullVehicleAplication/models/User.java
@@ -13,7 +13,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class User {
 		
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,44 +23,4 @@ public class User {
 	private String firstname;
 	private String lastname;
 
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
 }
