0d16ee0cbb10ed7158c34edbb7dc532f8399f794
Grzybatronix <mateusz.grzybowski.warszawa@gmail.com>
2020-12-20 10:51:26 +0100

    prepering database for heroku

---
+++ fullVehicleAplication/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into user values(1, 'Computer Science', 'Kindson Munonye', 'The Tech Pro', 'Data Science');
+insert into user values(2, 'Managment', 'Kate Winston',  'The Tech Pro', 'Data Science');
+insert into user values(3, 'Data Science', 'Solace Okeke', 'The Tech Pro', 'Data Science');
+insert into user values(4, 'Computer Science', 'Barack Obama',  'The Tech Pro', 'Data Science');
+insert into user values(5, 'US President', 'Donald Trump',  'The Tech Pro', 'Data Science');
+insert into user values(6, 'Data Science', 'Jadon Mills', 'The Tech Pro', 'Data Science');
+insert into user values(7, 'aaa', 'aaa', 'aaa', 'aaa');
